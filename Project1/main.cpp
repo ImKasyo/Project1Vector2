ecbb7b0918c6c6488ac56076ffa872b048229288
Mandi_pv66 <Mandi_pv66@student.itstep.org>
2024-10-01 23:51:09 +0300

    Вызвал методы в main. Все работает корректно

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -22,6 +22,26 @@ int main()
 	cout << "Last = " << last << endl;
 
 	
+    Vector myVector(3);
 
+    myVector.PuchFront(10);
+    myVector.PuchFront(20);
+    myVector.PuchFront(30);
+    cout << "После PuchFront:" << endl;
+    myVector.Print(); 
+
+    myVector.PushFront(2, 12); 
+    cout << "После PushFront (index 2, value 12):" << endl;
+    myVector.Print(); 
+
+    myVector.DeleteIndex(2); 
+    cout << "После DeleteIndex (index 2):" << endl;
+    myVector.Print(); 
+
+  
+    int addArr[] = { 40, 50 };
+    myVector.AddArrPushBack(addArr, 2);
+    cout << "После AddArrPushBack:" << endl;
+    myVector.Print(); 
 
 }
